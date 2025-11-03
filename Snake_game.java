df71002d416a87bbc8e73f698e1b4842651e5c47
aadxarsh <aadarshbhandaribsc24@rvu.edu.in>
2025-11-03 21:26:15 +0530

    Initialized GamePanel for drawing and movement logic

--- Snake_game.java
+++ Snake_game.java
@@ -2,3 +2,5 @@ import java.util.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+--------------------------------------------------------------
+public class GamePanel extends JPanel implements ActionListener  
