9999caf126bd4b3720d20308549112243dda265f
Krishnendu Nayak <krishnedunayakbsc24@rvu.edu.in>
2025-11-06 10:05:04 +0530

    Fix syntax errors and initialize game variables

--- GamePanel.java
+++ GamePanel.java
@@ -8,5 +8,9 @@ public class GamePanel extends JPanel implements ActionListener {
   private final int GRID_WIDTH = 25
   GRID_HEIGHT = 25;
   private final int PANEL_WIDTH = GRID_WIDTH * BOX_SIZE, PANEL_HEIGHT = GRID_HEIGHT * BOX_SIZE;
-  
-  
+  private LinkedList<Point> snake;
+  private Point food;
+  private char direction = 'R';
+  private boolean running = false;
+  private javax.swing.Timer timer;
+}  
