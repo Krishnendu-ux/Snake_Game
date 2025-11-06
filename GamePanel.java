a0bbbb651534b4b31357d8e3a1a6dd9ca69114f6
aadxarsh <aadarshbhandaribsc24@rvu.edu.in>
2025-11-06 09:08:53 +0530

    defining box size and grid dimenstions
    guys i have added box size and grid constants for layout setup

--- GamePanel.java
+++ GamePanel.java
@@ -3,4 +3,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 --------------------------------------------------------------
-public class GamePanel extends JPanel implements ActionListener  
+public class GamePanel extends JPanel implements ActionListener {
+  private final int BOX_SIZE = 20;
+  private final int GRID_WIDTH = 25
+  GRID_HEIGHT = 25;
+  private final int PANEL_WIDTH = GRID_WIDTH * BOX_SIZE, PANEL_HEIGHT = GRID_HEIGHT * BOX_SIZE;
+  
+  
