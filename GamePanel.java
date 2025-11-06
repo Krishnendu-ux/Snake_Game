812a9e144df36c8e395097352de0497621eee121
Krishnendu Nayak <krishnedunayakbsc24@rvu.edu.in>
2025-11-06 10:05:54 +0530

    Fix GRID_WIDTH and GRID_HEIGHT declaration

--- GamePanel.java
+++ GamePanel.java
@@ -5,8 +5,7 @@ import javax.swing.*;
 --------------------------------------------------------------
 public class GamePanel extends JPanel implements ActionListener {
   private final int BOX_SIZE = 20;
-  private final int GRID_WIDTH = 25
-  GRID_HEIGHT = 25;
+  private final int GRID_WIDTH = 25, GRID_HEIGHT = 25;
   private final int PANEL_WIDTH = GRID_WIDTH * BOX_SIZE, PANEL_HEIGHT = GRID_HEIGHT * BOX_SIZE;
   private LinkedList<Point> snake;
   private Point food;
