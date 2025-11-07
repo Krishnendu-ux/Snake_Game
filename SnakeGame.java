b7fbc17c14a86267d4819d96c53df5e3974dcd03
aryamankanoirvu <aryamankanoibsc24@rvu.edu.in>
2025-11-07 10:31:39 +0530

    Add constructor to SnakeGame class
    Initialize SnakeGame with a GamePanel and set title.

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,2 +1,8 @@
 import javax.swing.*;
 public class SnakeGame extends JFrame {
+  public SnakeGame() {
+      add(new GamePanel());
+      setTitle("Snake Game");
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
+    
